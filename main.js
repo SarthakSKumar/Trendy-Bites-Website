52668a45acb70989f09982aac7938b4e65cbb0bc
Sarthak S Kumar <sskworld9742@gmail.com>
2022-04-15 13:30:08 +0530

    Major Changes

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
-        if (scroll > 300) {
-            $(".navbar-top").css("background", "black");
-            $(".logo").css("height", "142px");
-            $(".logo").css("width", "186px");
-
+        if (scroll > 400) {
+            $(".navbar-top").css("background", "#0e0e0e");
+            $(".logo").css("height", "71px");
+            $(".logo").css("width", "93px");
+            $(".logo").css("margin-top", "5px");
+            $(".logo").css("margin-right", "250px");
+            $(".logo").css("transition", "0.7s");
             $(".navbar-top").css("transition", "0.3s");
         }
 
         else {
             $(".navbar-top").css("background", "transparent");
-            $(".navbar-top").css("transition", "0.3s");
+            $(".logo").css("height", "142px");
+            $(".logo").css("width", "186px");
+            $(".logo").css("margin-top", "80px");
+            $(".logo").css("margin-right", "200px");
         }
     })
 })
